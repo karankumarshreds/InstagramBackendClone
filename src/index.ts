abaf626ee7d2801190bcf98d21babca187053471
karankumarshreds <naileditnerd@gmail.com>
2021-06-03 09:35:52 +0530

    add environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,44 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// custom
+import app from './app';
+
+const PORT = process.env.PORT || 5000;
 
 const start = async () => {
+  // environment checks
+  if (
+    !process.env.DB_HOST ||
+    !process.env.DB_PORT ||
+    !process.env.DB_USER ||
+    !process.env.DB_PASSWORD ||
+    !process.env.DB_NAME
+  ) {
+    throw new Error(
+      '❌ Data base hostname, port, user, password and database name must be provided ❌'
+    );
+  }
+
   try {
-    const config = await createConnection({
+    await createConnection({
       name: 'instagram',
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'password',
-      database: 'Instagram',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT!),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     });
     console.log('Connected with DB 🎉');
+
+    // start the express app
+    app.listen(PORT, () => {
+      console.log('Listening on port 5000');
+    });
   } catch (error) {
     console.log(error);
   }
