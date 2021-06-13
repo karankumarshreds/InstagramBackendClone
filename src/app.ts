e4fbf107059fb98b6ef069c1268ef5ca5fbd1899
karankumarshreds <naileditnerd@gmail.com>
2021-06-13 10:37:25 +0530

    fix app setup

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import { json } from 'body-parser';
 
 // routes
 import { UserRouter } from './routes/user';
@@ -7,6 +8,7 @@ import { UserRouter } from './routes/user';
 const app = express();
 
 app.use(cors());
+app.use(json());
 app.use(UserRouter);
 
 export default app;
